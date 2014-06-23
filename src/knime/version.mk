b6bf97f21149168922c66d7be53792d442cdb0dc
Jerry Greenberg <jpg@sdsc.edu>
2014-06-23 14:09:57 -0700

    split knime into several packages

--- src/knime/version.mk
+++ src/knime/version.mk
@@ -1,6 +1,6 @@
 PKGROOT            = /opt/knime
 NAME               = knime
-VERSION            = 2.9.4
+VERSION            = 2.9.2
 RELEASE            = 0
 
 SRC_SUBDIR         = knime
@@ -8,7 +8,7 @@ SRC_SUBDIR         = knime
 SOURCE_NAME        = $(NAME)
 SOURCE_VERSION     = $(VERSION)
 SOURCE_SUFFIX      = tar.gz
-SOURCE_PKG         = $(SOURCE_NAME)_$(SOURCE_VERSION).linux.gtk.$(ARCH).$(SOURCE_SUFFIX)
+SOURCE_PKG         = $(SOURCE_NAME)_$(SOURCE_VERSION)_all_extensions.$(SOURCE_SUFFIX)
 SOURCE_DIR         = $(SOURCE_NAME)_$(SOURCE_VERSION:%.$(SOURCE_SUFFIX)=%)
 
 TAR_GZ_PKGS           = $(SOURCE_PKG)
--- src/linux.mk
+++ src/linux.mk
@@ -2,5 +2,5 @@ SRCDIRS = `find * -prune\
 	  -type d 	\
 	  ! -name CVS	\
           ! -name knime-modules \
-          ! -name build-knime-modules \
+          ! -name build-* \
 	  ! -name .` knime-modules
